03891f48216dce68ed6098223adc618cad2fd9b3
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-09-14 23:46:51 +0500

    Task1

---
+++ Tyuiu.PupovAA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,10 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Артём";
+        int age = 10;
+        Console.WriteLine("Здраствуйте! Меня зовут " + name + " мне " + age + " лет!");
+    }
+}
