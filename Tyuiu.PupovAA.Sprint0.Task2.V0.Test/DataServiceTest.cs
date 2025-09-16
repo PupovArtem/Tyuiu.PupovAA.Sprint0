a9d8ec0942e91f4348943fb774678d8cea61d6a2
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-09-17 04:01:37 +0500

    Task2

---
+++ Tyuiu.PupovAA.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.PupovAA.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.PupovAA.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMassageValid()
+        {
+            var name = "Артём";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Артём", res);
+        }
+    }
+}
---
+++ Tyuiu.PupovAA.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,9 @@
+﻿using Tyuiu.PupovAA.Sprint0.Task2.V0.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Артём"));
+        Console.ReadKey();
+    }
+}
